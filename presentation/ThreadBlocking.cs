2127af066765cb09e7cbbd3177c258ad1578cf95
danielmarbach <daniel.marbach@openplace.net>
2016-05-24 23:42:12 +0200

    Minor changes

--- presentation/ThreadBlocking.cs
+++ presentation/ThreadBlocking.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Transactions;
+using AsyncDolls;
 using NUnit.Framework;
 
 namespace RearchitectTowardsAsyncAwait
